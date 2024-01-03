edb3a1a30d7d7ce0efe569f0c69f5aeacca790b7
Jiaha0-Zhang <xihd2017@gmail.com>
2024-01-02 19:54:16 -0800

    BOOLEAN LOGIC SUBMISSION ASSIGNMENT

--- Basic approval program for car insurance/Basic approval program for car insurance/Program.cs
+++ Basic approval program for car insurance/Basic approval program for car insurance/Program.cs
@@ -11,15 +11,15 @@ namespace Basic_approval_program_for_car_insurance
         static void Main()
         {
             Console.WriteLine("What is your age?");
-            int age = Convert.ToInt32(Console.ReadLine());
+            int age = Convert.ToInt32(Console.ReadLine()); //input age
 
             Console.WriteLine("\n"+"Have you ever had a DUI? Please input 'True' or 'False'.");
-            bool haveDUL = Convert.ToBoolean(Console.ReadLine());
+            bool haveDUL = Convert.ToBoolean(Console.ReadLine()); //input true or false to determine haveDUL
 
             Console.WriteLine("\n" + "How many speeding tickets do you have?");
-            int tickets = Convert.ToInt32(Console.ReadLine());
+            int tickets = Convert.ToInt32(Console.ReadLine()); //input speeding tickests
 
-            bool isQualified = age > 15 && !haveDUL && tickets <= 3;
+            bool isQualified = age > 15 && !haveDUL && tickets <= 3;  //return true if age over 15 And have no DUL And no more than 3 tickets
             Console.WriteLine("\n"+ "Does the applicant qualify for car insurance" +"\n"+ isQualified);
             Console.ReadLine();
         }
