c24f3cadb2146735028630ea39666d6aa0b9d16e
Jiaha0-Zhang <xihd2017@gmail.com>
2024-01-06 22:30:00 -0800

    METHOD CLASS SUBMISSION ASSIGNMENT

---
+++ Assignments/Main Methods3/Main Methods3/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Methods3
+{
+    internal class Math
+    {
+        public void MathOperation(int a, int b)
+        {
+            int addFive = a + 5;
+            Console.WriteLine("Result of the first number plus 5 is: " + addFive);
+            Console.WriteLine("The second number is:" + b);
+        }
+    }
+}
---
+++ Assignments/Main Methods3/Main Methods3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Methods3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Math math = new Math(); //instantiation
+            Console.WriteLine("Call the method in the class, passing in two numbers.");
+            math.MathOperation(6, 8);
+
+            Console.WriteLine("\n"+ "Call the method in the class, specifying the parameters by name.");
+            math.MathOperation(a:7 , b: 10);
+
+
+            Console.ReadLine();
+        }
+    }
+}
