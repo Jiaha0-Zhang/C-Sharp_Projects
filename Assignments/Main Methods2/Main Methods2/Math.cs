c6e613f5da9e523082c9614ed514890a0b8c227b
Jiaha0-Zhang <xihd2017@gmail.com>
2024-01-06 22:14:17 -0800

    METHOD SUBMISSION ASSIGNMENT

---
+++ Assignments/Main Methods2/Main Methods2/Math.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Main_Methods2
+{
+    internal class Math
+    {
+        public int AdditionOperation(int a, int b = 0)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Assignments/Main Methods2/Main Methods2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Main_Methods2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Math math = new Math();
+
+            Console.WriteLine("Please input the first number");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input the second number or press 'Enter' button if you don't want to input the second number.");
+            string secondNum = Console.ReadLine();
+
+            if(string.IsNullOrEmpty(secondNum))
+            {
+                int result = math.AdditionOperation(firstNum);
+                Console.WriteLine("The result of first number plus nothing is: " + result);
+            }
+            else
+            {
+                int result = math.AdditionOperation(firstNum, int.Parse(secondNum));
+                Console.WriteLine("The result of first number plus second number is: " + result);
+            }
+
+           
+            Console.ReadLine();
+        }
+    }
+}
