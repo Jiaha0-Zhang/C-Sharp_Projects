9f5552c9674829debbaf30f2fea47e2815c931ec
Jiaha0-Zhang <xihd2017@gmail.com>
2024-01-06 19:54:13 -0800

    CALLING METHODS SUBMISSION ASSIGNMENT

---
+++ Assignments/Calling Methods/Calling Methods/MathOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+    internal class MathOperations
+    {
+        public int Square(int x)
+        {
+            return x * x;
+        }
+
+        public int Cube(int x)
+        {
+            return x * x * x;
+        }
+
+        public int MinusFive (int x)
+        {
+            return x - 5;
+        }
+
+        public int AddFive(int x)
+        {
+            return x + 5;
+        }
+    }
+}
---
+++ Assignments/Calling Methods/Calling Methods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter a whole number ");
+            int inputNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("--------------------------------");
+            MathOperations math = new MathOperations(); //Create an instance of the MathOperations class
+
+            int square = math.Square(inputNumber);  //call method by a parameter
+            Console.WriteLine(inputNumber + " squared is " + square);
+
+            int cube = math.Cube(inputNumber); //call method by a parameter
+            Console.WriteLine(inputNumber + " cubed is " + cube);
+
+            int minusFive = math.MinusFive(inputNumber); //call method by a parameter
+            Console.WriteLine(inputNumber +" minus Five squals " + minusFive);
+
+            int addFive = math.AddFive(inputNumber); //call method by a parameter
+            Console.WriteLine(inputNumber + " add Five squals " + addFive);
+
+            Console.ReadLine();
+        }
+    }
+}
