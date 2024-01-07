ac5b41836ad2e6d71bfcc5f76f65d86b4c854cf9
Jiaha0-Zhang <xihd2017@gmail.com>
2024-01-06 21:48:06 -0800

    MAIN METHOD SUBMISSION ASSIGNMENT

---
+++ Assignments/Main Methods/Main Methods/Math.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Methods
+{
+    internal class Math
+    {
+        public int MathOperation(int x)
+        {
+            return x + 5;
+        }
+
+        public int MathOperation(decimal x)
+        {
+            return (int)x + 5;
+        }
+
+        public int MathOperation(string x)
+        {
+            return int.Parse(x) * int.Parse(x);
+        }
+    }
+}
---
+++ Assignments/Main Methods/Main Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Methods
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Math math = new Math();// Instantiation 
+
+            int intAddFive = math.MathOperation(2); //Call the first method 
+            Console.WriteLine("Result of intAddFive is : "+intAddFive);
+
+            int decimalAddFive = math.MathOperation(3.6m); //Call the second method
+            Console.WriteLine("Result of decimalAddFive is: " + decimalAddFive);
+
+            int stringSquare = math.MathOperation("5"); //Call the third method
+            Console.WriteLine("Result of stringSquare is: " + stringSquare);
+
+            Console.ReadLine();
+        }
+    }
+}
