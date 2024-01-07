9c70e23a2c78e2bae197e9503c237e088fb625ac
Jiaha0-Zhang <xihd2017@gmail.com>
2024-01-06 23:06:37 -0800

    CLASS SUBMISSION ASSIGNMENT

---
+++ Assignments/Main Methods4/Main Methods4/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Methods4
+{
+    internal class Math
+    {
+        public void MathOperation(int x)
+        {
+            int result = x / 2;
+            Console.WriteLine("You have input the number " + "x" + ", the result is " + result);
+        }
+
+        public void MathOperation2(int x, out int result) //a method with output parameters.
+        {
+            result = x / 5;
+        }
+
+        public void MathOperation(int x, int y) //Overload a method.
+        {
+            int result = x + y;
+
+        }
+    }
+
+}
---
+++ Assignments/Main Methods4/Main Methods4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Methods4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Math math = new Math(); //instantiate the class.
+
+            Console.WriteLine("Please input a number");
+            int inputNum = Convert.ToInt32(Console.ReadLine());
+            math.MathOperation(inputNum);
+            
+            Console.ReadLine();
+        }
+    }
+
+    static class justAClass //Declare a class to be static.
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("This is a static class");
+        }
+    }
+}
