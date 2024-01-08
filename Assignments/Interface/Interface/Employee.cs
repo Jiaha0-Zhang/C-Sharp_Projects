cac185a3cf40305c3fd44171a66da9f796847a4e
Jiaha0-Zhang <xihd2017@gmail.com>
2024-01-08 01:23:17 -0800

    POLYMORPHISM SUBMISSION ASSIGNMENT

---
+++ Assignments/Interface/Interface/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal class Employee: Person, IQuittable
+    {
+        public void Quit()
+        {
+            Console.WriteLine(firstName + " " + lastName + " is quitting!");
+
+        }
+
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+    }
+}
---
+++ Assignments/Interface/Interface/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Employee employee = new Employee() { firstName = "Sam", lastName = "Smith" }; //instantiation
+
+            employee.Quit(); //call the interface
+
+            Console.ReadLine();
+        }
+    }
+}
