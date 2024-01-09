328d6f42bf4db42b1d59582ef608cbdb399295dd
Jiaha0-Zhang <xihd2017@gmail.com>
2024-01-08 20:15:07 -0800

    PARSING ENUMS SUBMISSION ASSIGNMENT

---
+++ Assignments/Enumeration/Enumeration/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeration
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week");
+                string enterText = Console.ReadLine();
+
+                DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), enterText); //another example: Colors orange = (Colors) Enum.Parse(typeof(Colors), "Red, Yellow");
+     
+                Console.WriteLine("The day of the week you entered is: " + day );
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignments/Enumeration/Enumeration/Weekdays.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeration
+{
+    public enum DaysOfTheWeek
+    {
+        Monday, Tuesday, Wednesday,
+        Thursday, Friday,
+        Saturday,Sunday
+    }
+}
