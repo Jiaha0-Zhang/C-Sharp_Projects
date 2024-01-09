6b8392a8f5e57e3e691d927676bcb9ccd12bad93
Jiaha0-Zhang <xihd2017@gmail.com>
2024-01-08 17:53:03 -0800

    OPERATORS SUBMISSION ASSIGNMENT

---
+++ Assignments/Overload Operator/Overload Operator/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload_Operator
+{
+    internal class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+
+        public static bool operator == (Employee em1, Employee em2)
+        {
+            return (em1.Id == em2.Id);
+        }
+
+        public static bool operator != (Employee em1, Employee em2)
+        { 
+            return !(em1.Id == em2.Id);
+        }
+    }
+}
---
+++ Assignments/Overload Operator/Overload Operator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee em1 = new Employee() { Id = 1, FirstName = "Sam", LastName = "Smith" }; //instantiation
+
+
+            Employee em2 = new Employee() { Id = 2, FirstName = "John", LastName = "Johnson" }; //instantiation
+
+            bool isMatched = em1 == em2;  // compare objects
+            Console.WriteLine(isMatched);
+
+            Console.ReadLine();
+        }
+    }
+}
