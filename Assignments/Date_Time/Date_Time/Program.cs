0d998bded2b6832d9eb2504d4604dcd9f0e4531a
Jiaha0-Zhang <xihd2017@gmail.com>
2024-01-11 16:46:05 -0800

    DATE/TIME SUBMISSION ASSIGNMENT

---
+++ Assignments/Date_Time/Date_Time/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_Time
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine("The current date and time is {0}  ", currentTime);
+
+            Console.WriteLine("Please enter a number.");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                if(number > 0)
+                {
+                    DateTime futureTime = currentTime.AddHours(number);
+                    Console.WriteLine("After {0} hours, it will be {1}", number, futureTime);
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
