1be1d3b23bb7e245bca66a0892fd19132285fafc
Jiaha0-Zhang <xihd2017@gmail.com>
2024-01-11 20:16:36 -0800

    CONSTRUCTOR SUBMISSION ASSIGNMENT

---
+++ Assignments/Chain_Constructors/Chain_Constructors/JayClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chain_Constructors
+{
+    public class JayClass
+    {
+        public string Name;
+        public int Age;
+
+        public JayClass(string name) : this(name, 20) //Chain two constructors together. 20 is the default value.
+        {
+
+        }
+
+        public JayClass(string name ,int age) 
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ Assignments/Chain_Constructors/Chain_Constructors/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chain_Constructors
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            const string name = "Jay"; //Create a const variable.
+
+            var age = 0; //Create a variable using the keyword “var.”
+
+
+
+            JayClass jay = new JayClass("Jay");
+            Console.WriteLine("{0} is a {1} years old student.", jay.Name, jay.Age); //Jay is a 20 years old student. Here jay.age is a default value 20.
+
+            JayClass someone = new JayClass("Ted",15);
+            Console.WriteLine("{0} is a {1} years old student.", someone.Name, someone.Age); //Ted is a 15 years old student.
+
+            Console.ReadLine();
+        }
+    }
+}
