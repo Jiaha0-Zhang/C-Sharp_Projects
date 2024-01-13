777970bd9cd891015f3eacd7f3d5609875d59e08
Jiaha0-Zhang <xihd2017@gmail.com>
2024-01-12 22:13:41 -0800

    PRACTICAL EXAMPLE USING LINQ

---
+++ Assignments/Practical Example Using LINQ/Practical Example Using LINQ/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_Example_Using_LINQ
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] students = new string[] { "Lacey", "Trisha", "Gavin", "Josh", "Joe", "Landon", "Kyndreshia" }; //Specify the data source.
+
+            IEnumerable<string> studentQuery = from student in students where student.Length >= 6 select student; //Define the query expression.
+
+            foreach (string student in studentQuery) //Execute the query.
+            {
+                Console.WriteLine(student + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
